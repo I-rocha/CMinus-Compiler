71a3d3030f8dec5c66d913304e645d2f0fbd5a21
Israel Rocha <israel.rexc@yahoo.com>
2023-06-27 18:08:37 -0300

    added list string

--- utils.c
+++ utils.c
@@ -40,3 +40,45 @@ void allocateValidator(void** ptr, const char* msg, const char* func){
 	printf("%s", err_txt);
 	exit(0);
 }
+
+listString newListString(){
+	listString ls;
+
+	ls.list = NULL;
+	ls.len = 0;
+	return ls;
+}
+
+int addListString(listString ls, char* str){
+	ls.list = (char**)realloc(ls.list, sizeof(char*) * (ls.len+1));
+	
+	if(!ls.list){
+		printf("Error, allocating didn't work (addListString)\n");
+		exit(0);
+		return -1;
+	}
+
+	ls.list[ls.len++] = strdup(str);
+	return 1;
+}
+
+int getKeyListString(listString ls, char* str){
+	for(int i = 0; i < ls.len; i++){
+		if(strcmp(ls.list[i], str) == 0)
+			return i;
+	}
+}
+
+void freeListString(listString ls){
+	if(!ls.list)
+		return;
+
+	for(int i = 0; i < ls.len; i++){
+		if(!ls.list[i])
+			continue;
+		free(ls.list[i]);
+	}
+	free(ls.list);
+	ls.list = NULL;
+	ls.len = 0;
+}
--- utils.h
+++ utils.h
@@ -5,10 +5,22 @@
 #define MALLOC_VALIDATE "Error, malloc allocation did not work properly", __func__
 #define REALLOC_VALIDATE "Error, realloc allocation did not work properly", __func__
 
+typedef struct{
+	char** list;
+	int len;
+}listString;
+
 char* lexformat(char* tok, char* lex);
 
-
+/* Check if ptr was indeed allocated */
 void allocateValidator(void** ptr, const char* msg, const char* func);
+
+/* Free pointer and set null */
 void freeNull(void** ptr);
+
+listString newListString();
+int addListString(listString ls, char* str);
+int getKeyListString(listString ls, char* str);
+void freeListString(listString ls);
 
 #endif
